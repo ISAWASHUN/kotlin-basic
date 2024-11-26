1b7f22caa822bab8fdfa000843a019dc3d4e67b9
ISAWASHUN <syun20000616@icloud.com>
2024-11-26 21:42:39 +0900

    coroutineの実装

---
+++ coroutine.kt
@@ -0,0 +1,9 @@
+// import kotlinx.coroutines.*
+
+// fun main() = runBlocking {
+//     launch {
+//         delay(1000L) // 1秒間一時停止
+//         println("Hello, Coroutines!")
+//     }
+//     println("Start")
+// }
