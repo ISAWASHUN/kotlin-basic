13fec2ca1470ac21466a1646cb39676041177909
ISAWASHUN <syun20000616@icloud.com>
2024-12-02 00:01:07 +0900

    インラインクラス

---
+++ InlineClass.kt
@@ -0,0 +1,21 @@
+// Good Code
+@JvmInline
+value class UserId(val id: String)
+
+@JvmInline
+value class ProductId(val id: String)
+
+fun getUserById(userId: UserId): String {
+  return "User with ID: ${userId.id}"
+}
+
+fun getProductById(productId: ProductId): String {
+  return "Product with ID: ${productId.id}"
+}
+
+val userId = UserId("user123")
+val productId = ProductId("product456")
+println(getUserById(userId))
+println(getProductById(productId))
+
+kk
